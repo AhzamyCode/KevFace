b9563f44f1c99995bbfb73af54fffbd7d389b145
Ahzamycode <ahzamycode@gmail.com>
2025-10-19 11:33:46 +0700

    first commit wlwlw

---
+++ src/app/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      <footer className='text-sm text-center italic text-green-600'>
+        <p>&copy; 2025 KevFace. Berdasarkan ai cuma jangan percaya wirr </p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
